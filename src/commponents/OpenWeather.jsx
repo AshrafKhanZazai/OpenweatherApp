d5e7f284d974a041213851edabc2716a5494c8d3
unknown <ashrafkhanzazai8@gmail.com>
2024-08-13 11:33:38 +0430

    weathers Icons changed

--- src/commponents/OpenWeather.jsx
+++ src/commponents/OpenWeather.jsx
@@ -1,33 +1,33 @@
 import { useState, useEffect, useRef } from 'react';
 import search_icon from "../Assets/search.jpg"
 import clear_icon from "../Assets/clear.png"
-// import cloud_icon from "../Assets/cloud.png"
-// import drizzle_icon from "../Assets/drizzle.png"
-// import rain_icon from "../Assets/rain.png"
-// import snow_icon from "../Assets/snow.png"
-// import wind_icon from "../Assets/wind.png"
+import cloud_icon from "../Assets/cloud.png"
+import drizzle_icon from "../Assets/drizzle.png"
+import rain_icon from "../Assets/rain.png"
+import snow_icon from "../Assets/snow.png"
+import wind_icon from "../Assets/wind.png"
 import "../App.css"
 
 const OpenWeather = () => {
-const inputRef = useRef();
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState('New York');
-  const [apiKey, setApiKey] = useState('3f4024f91952fe5a8eefa002a50c8e62');
-
-//   const allIcons = {
-//     "01d" : clear_icon,
-//     "01n" : clear_icon,
-//     "02d" : cloud_icon,
-//     "02n" : cloud_icon,
-//     "03d" : drizzle_icon,
-//     "03n" : drizzle_icon,
-//     "04d" : rain_icon,
-//     "04n" : rain_icon,
-//     "09d" : snow_icon,
-//     "09n" : snow_icon,
-//     "10d" : wind_icon,
-//     "10n" : wind_icon
-//   }
+  const [apiKey, setapiKey] = useState('3f4024f91952fe5a8eefa002a50c8e62');
+  const inputRef = useRef();
+  
+  const iconMap = {
+    "01d" : clear_icon,
+    "01n" : clear_icon,
+    "02d" : cloud_icon,
+    "02n" : cloud_icon,
+    "03d" : drizzle_icon,
+    "03n" : drizzle_icon,
+    "04d" : rain_icon,
+    "04n" : rain_icon,
+    "09d" : snow_icon,
+    "09n" : snow_icon,
+    "10d" : wind_icon,
+    "10n" : wind_icon
+  }
 
   useEffect(() => {
       const fetchWeatherData = async () => {
@@ -49,15 +49,19 @@ const inputRef = useRef();
 fetchWeatherData();
 }, [city, apiKey]);
 
+
   if (!weatherData) {
     return <div className='flex items-center justify-center' style={{fontSize: "25px", paddingTop: "300px", color: "red", fontWeight: "700"}}>Loading....</div>;
   }
+  
   const timestamp = weatherData.sys.sunset;
   const date = new Date(timestamp * 1000).toLocaleString();
 
-  const iconCode = weatherData.weather[0].icon;
-  const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+  // const iconCode = weatherData.weather[0].icon;
+  // const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+  const iconPath = iconMap[weatherData.weather[0].icon] || 'images/default.png';
 
+  
   return (
     // <div>
     //   <h2>Weather Forecast for {weatherData.name}</h2>
@@ -87,7 +91,7 @@ fetchWeatherData();
           <div className="font-bold text-xl">{weatherData.name}</div>
           <div className="text-sm text-gray-500">{date}</div>
           <div className="mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-24">
-           <img  style={{ width: '100rem', height: '170px' }} src={iconUrl} alt={weatherData.weather[0].description} />
+           <img  style={{ width: '200px', height: '200px' }} src={iconPath} alt={weatherData.weather[0].description} />
           </div>
           <div className="flex flex-row items-center justify-center mt-6">
             <div className="font-medium text-6xl">{weatherData.main.temp}°C</div>
@@ -126,5 +130,4 @@ fetchWeatherData();
       </div>
   );
 };
-
 export default OpenWeather
